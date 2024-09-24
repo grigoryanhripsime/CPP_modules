9a90fc162f79217da84446fa3e0e85704f5a59f3
Hripsime Grigoryan <hrigrigo@c1r1s5.42yerevan.am>
2024-09-24 17:41:18 +0400

    ex02 added

---
+++ cpp_module_02/ex02/Fixed.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int num;
+		static const int flactional_bits = 8;
+	public:
+		Fixed();
+		Fixed(const int);
+		Fixed(const float);
+		Fixed(const Fixed &fixed);
+		Fixed &operator=(const Fixed &fixed);
+		~Fixed();
+		int getRawBits (void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+		bool operator>(const Fixed &other) const;
+		bool operator<(const Fixed &other) const;
+		bool operator<=(const Fixed &other) const;
+		bool operator>=(const Fixed &other) const;
+		bool operator==(const Fixed &other) const;
+		bool operator!=(const Fixed &other) const;
+		Fixed operator+(const Fixed &other) const;
+		Fixed operator-(const Fixed &other) const;
+		Fixed operator*(const Fixed &other) const;
+		Fixed operator/(const Fixed &other) const;
+		Fixed &operator++(); //pre-increment
+		Fixed operator++(int); //post-increment
+		static Fixed& min(Fixed& first, Fixed& second);
+		static Fixed& min(const Fixed& first, const Fixed& second);
+		static Fixed& max(Fixed& first, Fixed& second);
+		static Fixed& max(const Fixed& first, const Fixed& second);
+
+};
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
