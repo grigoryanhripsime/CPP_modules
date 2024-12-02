174da2ed8ec23e06994bb481292dd88ff79acabe
Hripsime Grigoryan <hrigrigo@c1r2s3.42yerevan.am>
2024-12-02 17:32:19 +0400

    ex00 reviewed

--- cpp_module_03/ex00/main.cpp
+++ cpp_module_03/ex00/main.cpp
@@ -2,11 +2,18 @@
 
 int main(void)
 {
-	ClapTrap claptrap;
+	ClapTrap claptrap("eee");
 	ClapTrap n = claptrap;
-	// std::cout<<claptrap.name<<" "<<claptrap.hit_points<<" "<<claptrap.energy_points<<" "<<claptrap.attack_damage<<"\n";
+	ClapTrap n2;
 	claptrap.attack("aaa");
-	claptrap.takeDamage(-5);
-	claptrap.beRepaired(10);
 	claptrap.takeDamage(5);
+	claptrap.beRepaired(2147483648);
+	claptrap.beRepaired(2147483648);
+	claptrap.beRepaired(2147483648);
+	n2 = claptrap;
+	// std::cout<<claptrap.name<<" "<<claptrap.hit_points<<" "<<claptrap.energy_points<<" "<<claptrap.attack_damage<<"\n";
+	n2.takeDamage(1000);
+	claptrap.takeDamage(16);
+	// std::cout<<n2.name<<" "<<n2.hit_points<<" "<<n2.energy_points<<" "<<n2.attack_damage<<"\n";
+	// std::cout<<claptrap.name<<" "<<claptrap.hit_points<<" "<<claptrap.energy_points<<" "<<claptrap.attack_damage<<"\n";
 }
