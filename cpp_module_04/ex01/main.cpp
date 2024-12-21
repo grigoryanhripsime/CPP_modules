46127fac6f968c55401de9e171d18d0a2794f957
Hripsime Grigoryan <hrigrigo@c2r2s1.42yerevan.am>
2024-12-21 19:15:02 +0400

    main updated

--- cpp_module_04/ex01/main.cpp
+++ cpp_module_04/ex01/main.cpp
@@ -6,17 +6,6 @@
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-	std::cout<<"\nex01\n";
-
 	Cat cat1;
 	Cat cat2;
 	cat1 = cat2;
