f6060dde5a4b82392c1a1a8a1ca998e8a610a622
Hripsime Grigoryan <hrigrigo@c2r2s1.42yerevan.am>
2024-12-21 19:18:22 +0400

    main updated

--- cpp_module_04/ex01/main.cpp
+++ cpp_module_04/ex01/main.cpp
@@ -6,13 +6,18 @@
 
 int main()
 {
-	Cat cat1;
-	Cat cat2;
-	cat1 = cat2;
-	cat2.setIdeas(0, "hello");
-	cat1.setIdeas(0, "barev");
+	// Cat cat1;
+	// Cat cat2;
+	// cat1 = cat2;
+	// cat2.setIdeas(0, "hello");
+	// cat1.setIdeas(0, "barev");
 	// std::cout<<cat1.brain->ideas[0]<<std::endl;
 	// std::cout<<cat2.brain->ideas[0]<<std::endl;
+
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
 	
 	//TODO: add more cases
 	return 0;
