a1938ec33a511634d4281a7b2010089cba428b82
Hripsime Grigoryan <hrigrigo@c2r2s1.42yerevan.am>
2024-12-23 20:07:04 +0400

    AMateria done

---
+++ cpp_module_04/ex03/AMateria.cpp
@@ -0,0 +1,37 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type)
+{
+	std::cout<<"AMateria ctor w/params called!\n";
+	this -> type = type;
+}
+
+AMateria::AMateria()
+{
+	std::cout<<"AMateria default ctor called!\n";
+}
+
+AMateria::AMateria(const AMateria &other)
+{
+	std::cout<<"AMateria copy ctor called!\n";
+	this -> type = other.type;
+}
+
+AMateria &AMateria::operator=(const AMateria &other)
+{
+	if (this == &other)
+		return *this;
+	std::cout<<"AMateria sopy assignment operator called!\n";
+	this -> type = other.type;
+	return *this;
+}
+
+AMateria::~AMateria()
+{
+	std::cout<<"AMateria dtor called!\n";
+}
+
+std::string const &AMateria::getType() const
+{
+	return this -> type;
+}
---
+++ cpp_module_04/ex03/AMateria.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class AMateria
+{
+	protected:
+		std::string type;
+	public:
+		AMateria(std::string const &type);
+		//Orthodox
+		AMateria();
+		AMateria(const AMateria &other);
+		AMateria &operator=(const AMateria &other);
+		~AMateria();
+
+		std::string const &getType() const; //Returns the materia type
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
