eaef8234f0445bb3024a7da5c97c8a2212bb77e2
Hripsime <grigoryanhripsime18@gmail.com>
2025-01-18 14:01:06 +0400

    added

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -12,4 +12,13 @@ void iter(T arr[], size_t len, void (*f)(T &))
 	}
 }
 
+template <typename T>
+void iter(const T arr[], size_t len, void (*f)(const T &))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		f(arr[i]);
+	}
+}
+
 #endif
--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -6,15 +6,60 @@ void ft_abs(int &num)
 		num *= -1;
 }
 
+void f(const std::string &str)
+{
+	std::cout<<str.size()<<std::endl;
+}
+
 int main()
 {
-	int arr[] = {-1, 2, -3, 6, 7};
-	std::cout<<"arr before iter\n";
-	for (int i = 0; i < 5; i++)
-		std::cout<<arr[i]<<" ";
-	iter(arr, 5, &ft_abs);
-	std::cout<<std::endl<<"arr before iter\n";;
-	for (int i = 0; i < 5; i++)
-		std::cout<<arr[i]<<" ";
-	std::cout<<std::endl;
+	{	
+		int arr[] = {-1, 2, -3, 6, 7};
+		std::cout<<"arr before iter\n";
+		for (int i = 0; i < 5; i++)
+			std::cout<<arr[i]<<" ";
+		iter(arr, 5, &ft_abs);
+		std::cout<<std::endl<<"arr before iter\n";;
+		for (int i = 0; i < 5; i++)
+			std::cout<<arr[i]<<" ";
+		std::cout<<std::endl;
+	}
+
+	{
+		const std::string arr[] = {"hello", "bye"};
+		iter(arr, 2, &f);
+	}
+
 }
+
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
