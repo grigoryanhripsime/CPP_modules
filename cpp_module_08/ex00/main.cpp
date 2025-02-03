06b705320ac0449c3b9cb8a2f17f87927fc709fd
Hripsime <grigoryanhripsime18@gmail.com>
2025-02-03 23:54:12 +0400

    main.cpp changed

--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <deque>
 #include <easyfind.hpp>
 
 int main() {
-    std::vector<int> v;
+    std::deque<int> v;
     for (int i = 0; i < 10; i++)
         v.push_back(i);
 
-    std::vector<int>::iterator it = easyfind(v, 8);
+    std::deque<int>::iterator it = easyfind(v, 8);
     std::cout << it - v.begin()<<std::endl;
 
     try
     {
-        std::vector<int>::iterator it = easyfind(v, 10);
+        it = easyfind(v, 10);
         std::cout << it - v.begin()<<std::endl;
     } catch (const std::exception &e)
     {
